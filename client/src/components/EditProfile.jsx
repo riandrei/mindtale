ecd74494ab1e5a625149791ec145a3028c92baf6
DowelleDayle <156673102+DowelleDayle@users.noreply.github.com>
2024-03-18 10:14:11 +0000

    unfinish setting2

--- client/src/components/EditProfile.jsx
+++ client/src/components/EditProfile.jsx
@@ -5,17 +5,22 @@ import User from '../assets/Dowelle.jpg'
 
 export function EditProfile(props) {
     const [name, setName] = useState("");
-    
     const handleNameChange = (e) => {
         const newName = e.target.value;
         console.log(newName);
         setName(newName);
     };
+
     const [tag, setTag] = useState("#");
-    
     const handleTagChange = (e) => {
         const newTag = e.target.value;
         setTag("#" + newTag.substring(1, 5));
+    };
+
+    const [bio, setBio] = useState("");
+    const handleBioChange = (e) => {
+        const newBio = e.target.value;
+        setBio(newBio);
     };
 
     return (
@@ -35,7 +40,7 @@ export function EditProfile(props) {
                         maxLength={15} 
                         value={name} 
                         onChange={handleNameChange} 
-                        style={{ borderColor: name.length === 15 ? '#ff4f4f' : '' }}
+                        style={{ borderColor: name.length === 15 ? '#ff4f4f50' : '' }}
                     />
                     <span className={name.length === 15 ? styles.Length2 : styles.Length}>{name.length}/15</span>
                 </div>
@@ -47,7 +52,7 @@ export function EditProfile(props) {
                         type="text"
                         value={tag} 
                         onChange={handleTagChange}  
-                        style={{ borderColor: tag.length === 5 ? '#ff4f4f' : '' }}
+                        style={{ borderColor: tag.length === 5 ? '#ff4f4f50' : '' }}
                     />
                     <span className={tag.length === 5 ? styles.Length2 : styles.Length}>{tag.length - 1}/4</span>
                 </div>
@@ -55,7 +60,8 @@ export function EditProfile(props) {
 
             <div className={styles.Change_bio}>
                 <label for="Bio" htmlFor="">Bio:</label>
-                <textarea id="Bio" cols="10" rows="5"></textarea>
+                <textarea className={bio.length === 150 ? styles.textarea : ''} maxLength={150} value={bio} onChange={handleBioChange} id="Bio" cols="10" rows="5" ></textarea>
+                <span className={bio.length === 150 ? styles.Length2 : styles.Length}>{bio.length}/150</span>
             </div>
         </div>
     )
