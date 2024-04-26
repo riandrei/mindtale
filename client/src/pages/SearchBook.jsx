af7ef29abb621d9a49ea5cffc5ebd0773a5a2212
DowelleDayle <156673102+DowelleDayle@users.noreply.github.com>
2024-04-26 02:45:23 +0000

    99% searchbook

--- client/src/pages/SearchBook.jsx
+++ client/src/pages/SearchBook.jsx
@@ -1,4 +1,4 @@
-import React from 'react'
+import React,{useState} from 'react'
 import { useNavigate } from 'react-router-dom';
 import styles from '../css/SearchBook.module.css'
 
@@ -13,6 +13,12 @@ export function SearchBook(props) {
     const goBack = () => {
         navigate(-1);
     }
+
+    const [noBook, setNoBook] = useState(false);
+    const handleBookSearch = () => {
+        setNoBook( !noBook)
+    }
+
     return (
         <div className={styles.SearchBook}>
             <div className={styles.Top}>
@@ -21,8 +27,23 @@ export function SearchBook(props) {
                 <button>Search</button>
             </div>
             <div className={styles.Stories_con}>
-                {/* <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/> */}
-                <h1 className={styles.Oops}>Looks like the book you are looking for doesn't exist...</h1>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+                <SpecificStory title="Magic and Mascles" image={Story1} genre="Drama"/>
+
+                {/* popup if wala yung book na hinahanap niya */}
+                {
+                    noBook && <h1 className={styles.Oops}>Looks like the book you are looking for doesn't exist...</h1>
+                }
             </div>
         </div>
     )
