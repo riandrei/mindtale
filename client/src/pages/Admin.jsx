4381326f2cd45b3afb2dac95875d30e49f1a49d2
DowelleDayle <156673102+DowelleDayle@users.noreply.github.com>
2024-04-27 09:32:38 +0000

    kinda finish admin

--- client/src/pages/Admin.jsx
+++ client/src/pages/Admin.jsx
@@ -1,4 +1,5 @@
-import React from 'react'
+import React, {useState} from 'react'
+import { useNavigate } from 'react-router-dom'
 import styles from '../css/Admin.module.css'
 
 import Nav from '../components/Nav'
@@ -7,22 +8,37 @@ import AllStories from '../components/AllStories'
 import Back from '../assets/back.png'
 import Lucy from '../assets/Luvy.jpg'
 import Prev from '../assets/prev.png'
+import Next from '../assets/next.png'
 import Lock from '../assets/padlock.png'
 import Line from '../assets/line3.png'
 
 export function Admin() {
     
+    const navigate = useNavigate();
+    const goBack = () => {
+        navigate(-1)
+    }
+    const [selectedButton, setSelectedButton] = useState(null);
 
+    const handleButtonClick = (buttonType) => {
+      setSelectedButton(buttonType);
+    };
+
+    const[popUp, setPopUp] = useState(false)
+    const handlePopupClick = () => {
+        setPopUp( !popUp)
+    }
     return (
         <div className={styles.Admin}>
             <Nav />
-            <img className={styles.Back} src={Back} />
+            <img onClick={goBack} className={styles.Back} src={Back} />
             <div className={styles.Middle}>
                 <div className={styles.Middle_left}>
                     <img className={styles.Cover} src={Lucy} />
                     <div className={styles.Middle_left_down}>
                         <img src={Prev} />
                         <button>Generate</button>
+                        <img src={Next} />
                     </div>
                 </div>
 
@@ -82,19 +98,40 @@ export function Admin() {
                         </div>
                     </div>
                     <div className={styles.Story_type}>
-                        <button>Free access</button>
-                        <button className={styles.Premium}>
+                        <button
+                            className={`${styles.button} ${selectedButton === 'free' ? styles.select : ''}`}
+                            onClick={() => handleButtonClick('free')}
+                        >
+                            Free access
+                        </button>
+                        <button
+                            className={`${styles.button} ${selectedButton === 'premium' ? styles.select : ''}`}
+                            onClick={() => handleButtonClick('premium')}
+                        >
                             <img src={Lock} />
                             <span>Premium access</span>
                         </button>
                     </div>
-                    <button className={styles.Create}>Create Story</button>
+                    <button onClick={handlePopupClick} className={styles.Create}>Create Story</button>
                 </div>
             </div>
-            <img className={styles.Line} src={Line}/>
+            <div className={styles.Line_con}>
+                <img className={styles.Line} src={Line}/>
+            </div>
             <div className={styles.All_stories}>
                 <AllStories />
             </div>
+            {
+                popUp && (
+                    <div className={styles.Popup}>
+                        <h1>Are you sure you want to publish this story?</h1>
+                        <div className={styles.Sure_con}>
+                            <button>Publish</button>
+                            <button onClick={handlePopupClick}>Cancel</button>
+                        </div>
+                    </div>
+                ) 
+            }
         </div>
     )
 }
