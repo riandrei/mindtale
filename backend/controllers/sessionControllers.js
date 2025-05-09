beb4c449611a28a478d61100adf975499997b96d
Riandrei Casañas <riandrei.casanas@gmail.com>
2025-05-09 12:34:40 +0800

    Fixes

--- backend/controllers/sessionControllers.js
+++ backend/controllers/sessionControllers.js
@@ -12,7 +12,7 @@ const Story = require("../models/Story");
 const generativeAI = new GoogleGenerativeAI(process.env.GEMINI_API_KEY);
 
 const model = generativeAI.getGenerativeModel({
-  model: "gemini-1.5-pro",
+  model: "gemini-2.5-pro-exp-03-25",
 });
 
 module.exports.startSession = async function startSession(req, res) {
